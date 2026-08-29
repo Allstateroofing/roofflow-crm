287a1933db95fb60011f01c475ac03f91bba5fec
Nada Berdynaj <berdynaj.nada@gmail.com>
2026-08-29 11:35:55 -0400

    test resend email

--- app/api/send-notification/route.ts
+++ app/api/send-notification/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
     }
 
     const { data, error } = await resend.emails.send({
-      from: "All State Roofing <notifications@allstateroofing.com>",
+      from: "All State Roofing <onboarding@resend.dev>",
       to: [to],
       subject,
       html: `
